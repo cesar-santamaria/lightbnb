aba180c4b16b992a49fe747f941b0348a91cfe1b
Cesar Santamaria <cesar-santamaria@outlook.com>
2022-05-05 09:39:45 -0500

    add insert data for each table within database

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -16,7 +16,7 @@ CREATE TABLE properties (
   title VARCHAR(255) NOT NULL,
   description TEXT,
   thumbnail_photo_url VARCHAR(255) NOT NULL,
-  cover_photo VARCHAR(255) NOT NULL,
+  cover_photo_url VARCHAR(255) NOT NULL,
   cost_per_night INTEGER NOT NULL DEFAULT 0, 
   parking_spaces INTEGER NOT NULL DEFAULT 0, 
   number_of_bathrooms INTEGER NOT NULL DEFAULT 0, 
@@ -41,7 +41,7 @@ CREATE TABLE property_reviews (
   id SERIAL PRIMARY KEY NOT NULL,
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
-  reservations_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
   rating SMALLINT NOT NULL DEFAULT 0,
   message TEXT
 );
