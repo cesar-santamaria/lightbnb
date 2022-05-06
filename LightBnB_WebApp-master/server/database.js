929c605e33b1b162033618e33254a88f2cbaed92
Cesar Santamaria <cesar-santamaria@outlook.com>
2022-05-06 02:21:11 -0500

    refactor getAllProperties function to accept arguments enabling filtering

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -119,16 +119,64 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
- const getAllProperties = (options, limit = 10) => {
+ const getAllProperties = function(options, limit = 10) {
+  const queryParams = [];
  
-  return pool.query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then(res => {
-      return res.rows;
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  LEFT JOIN property_reviews ON properties.id = property_id
+  `;
+
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+
+  if (options.owner_id) {
+    queryParams ? queryString += `AND ` : queryString += `WHERE `;
+    queryParams.push(Number(options.owner_id));
+    queryString += `owner_id = $${queryParams.length} `;
+  }
+
+  if (options.minimum_price_per_night) {
+    queryParams ? queryString += `AND ` : queryString += `WHERE `;
+    queryParams.push(Number(options.minimum_price_per_night) * 100);
+    queryString += `cost_per_night >= $${queryParams.length} `;
+  }
+
+  if (options.maximum_price_per_night) {
+    queryParams ? queryString += `AND ` : queryString += `WHERE `;
+    queryParams.push(Number(options.maximum_price_per_night) * 100);
+    queryString += `cost_per_night <= $${queryParams.length} `;
+  }
+
+  if (options.minimum_rating) {
+    queryParams ? queryString += `AND ` : queryString += `WHERE `;
+    queryParams.push(Number(options.minimum_rating));
+    queryString += `rating >= $${queryParams.length} `;
+  }
+
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  console.log(queryString, queryParams);
+
+  return pool
+    .query(queryString, queryParams)
+    .then((result) => {
+      // console.log('result.rows: ', result.rows);
+      return result.rows;
     })
     .catch((err) => {
-      console.log(err.message);
+      return err.message;
     });
 };
+
 exports.getAllProperties = getAllProperties;
 
 /**
